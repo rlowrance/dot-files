a8802d5ec98fd879a8180aa2dac2e9e6c4b830e7
Roy Lowrance <roy.lowrance@gmail.com>
2013-08-04 11:57:59 -0400

    rename scripts

--- gather-dots-universal.sh
+++ gather-dots-universal.sh
@@ -1,6 +1,8 @@
 # copy dot files that are not system dependent to this directory
-cp ~/.bashrc .basrc
+cp ~/.bashrc .bashrc
 cp ~/.gvimrc .gvimrc
 cp ~/.vimrc .vimrc
 cp ~/.tmux.conf .tmux.conf
+cp -rf ~/.vim .vim
+cp -rf ~/.emacs.d .emacs.d
 
