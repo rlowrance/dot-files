8b35275117d403aa902d30d7caddfd7e3a18b83c
Roy Lowrance <roy@roy-VirtualBox.(none)>
2013-10-01 21:52:58 -0400

    add .bash_profile

--- install-dots-common.sh
+++ install-dots-common.sh
@@ -1,5 +1,6 @@
 # install dot files that don't depend on the system
 # copy individual files to home directory
+/bin/cp -r .bash_profile ~
 /bin/cp -r .inputrc ~
 /bin/cp -r .emacs ~
 /bin/cp -r .gvimrc ~
