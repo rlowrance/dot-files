7ae3047c621076684a29e7ee7fa8119fd83c77f8
Roy Lowrance <roy.lowrance@gmail.com>
2018-02-05 09:02:09 -0500

    split out aliases

--- dell7810-dot-bashrc.sh
+++ dell7810-dot-bashrc.sh
@@ -1,18 +1,5 @@
-# edit a file
-alias e7chord="emacs ~/Dropbox/ads/clients/7chord/7chord-01/7chord-meeting-notes.org &"
-alias ebashrc="emacs ~/Dropbox/control/dot-files/dell7810-dot-bashrc.sh &"
-alias ecti="emacs ~/Dropbox/ads/clients/cti/cti-meetings-notes.org &"
-alias eemacs="emacs ~/Dropbox/control/dot-files/dell7810-dot-emacs &"
-alias emaybe="emacs ~/Dropbox/control/todo/maybe.org &"
-alias etimesheet="emacs ~/Dropbox/control/timesheet/timesheet.txt &"
-alias etracking="emacs ~/Dropbox/control/todo/tracking.org &"
-
-# go to a directory
-alias gaws="cd ~/Dropbox/ads/clients/7chord/7chord-01/aws"
-alias gdots="cd ~/Dropbox/control/dot-files"
-
-# ssh to a server
-alias sshfp="bash ~/Dropbox/ads/clients/7chord/7chord-01/aws/sshfp.sh"
+# aliases
+source ~/Dropbox/control/dot-files/dell7810-aliases.sh
 
 # Julia lang
 PATH=$PATH:$HOME/bin/julia-d386e40c17/bin
